79e148f5218e69fb51bb7871161012fcc27d6fcc
SANGHYUN LEE <smarthyun88@lotte.net>
2023-04-06 17:32:02 +0900

    first commit

---
+++ src/main/java/com/example/springbootbasedspringbatch/SpringBootBasedSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbasedspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasedSpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBasedSpringBatchApplication.class, args);
+    }
+
+}
